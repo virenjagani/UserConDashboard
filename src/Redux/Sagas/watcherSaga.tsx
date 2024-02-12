39a8cbc198e3a6da0b815f2754bff641e6a190b9
Virenkumar Jagani <143466608+virenjagani@users.noreply.github.com>
2024-02-12 04:07:31 -0500

    userHandle

--- src/Redux/Sagas/watcherSaga.tsx
+++ src/Redux/Sagas/watcherSaga.tsx
@@ -15,12 +15,22 @@ import {
 } from "./Handlers/UserHandlers";
 
 function* userHandle() {
+  // Watch for GET_USER_ALL action and call fetchUserAll saga
   yield takeLatest(GET_USER_ALL, fetchUserAll);
+
+  // Watch for GET_USER_ADD action and call fetchUserAdd saga
   yield takeLatest(GET_USER_ADD, fetchUserAdd);
+
+  // Watch for GET_USER_DELETE action and call fetchUserDelete saga
   yield takeLatest(GET_USER_DELETE, fetchUserDelete);
+
+  // Watch for GET_USER_OBJECT action and call fetchUserObject saga
   yield takeLatest(GET_USER_OBJECT, fetchUserObject);
+
+  // Watch for GET_USER_EDIT action and call fetchUserEdit saga
   yield takeLatest(GET_USER_EDIT, fetchUserEdit);
 }
+
 
 const userHandleSaga = [fork(userHandle)];
 
